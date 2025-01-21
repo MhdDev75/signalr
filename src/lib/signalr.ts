45419b5763fe3553710cce504db19bd01b4acf08
Mahdi Ebrahimpour <149631776+MhdDev75@users.noreply.github.com>
2025-01-21 17:42:10 +0330

    add local host

--- src/lib/signalr.ts
+++ src/lib/signalr.ts
@@ -3,7 +3,7 @@ import { HubConnectionBuilder, HubConnection } from '@microsoft/signalr';
 
 
 const connection: HubConnection = new HubConnectionBuilder()
-    .withUrl("http://64.44.167.150:7001/hubs/openai", {
+    .withUrl("http://localhost:7001/hubs/openai", {
         accessTokenFactory: () => Promise.resolve("eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJodHRwOi8vc2NoZW1hcy54bWxzb2FwLm9yZy93cy8yMDA1LzA1L2lkZW50aXR5L2NsYWltcy9uYW1laWRlbnRpZmllciI6ImNhYjM5Y2UyLTMxMGMtNDEzNC05MWEzLTA4ZjYyNTgyMmU2ZiIsImV4cCI6MTczNzQ3MTc3MSwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDoyODc0Ny8iLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjI4NzQ3LyJ9.nqrERDRUVC5XBkD0z8Vaj7r0OaWIqkZeemLX4f0n7C4")
     })
     .build();
